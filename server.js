ff9b722725484cf370a8d18d976b60652e4b3dc3
Quentin Young <youngqp3@gmail.com>
2021-12-07 08:40:46 -0800

    socket io partially implemented

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ const mongoose = require("mongoose");
 const http = require("http");
 const app = express();
 const server = http.createServer(app);
+const cors = require('cors');
+
+const ChatRoom = require('./model/user.model') 
+
+app.use(cors())
 
 const { Server } = require("socket.io");
 const io = new Server(server);
@@ -16,13 +21,53 @@ const mongooseOptions = {
   useUnifiedTopology: true,
 };
 
-app.get("/");
+//CORS
+app.use(function(req, res, next) {
+  console.log("CORS")
+  res.header("Access-Control-Allow-Origin", "*");
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.header("Access-Control-Allow-Headers", "*");
+  next();
+});
+
+app.get("/:email", async (req, res) => {
+  console.log("GET!")
+  let email = req.params.email;
+  const publicDb = await ChatRoom.find({isPublic:true})
+  const privateDb = await ChatRoom.find({isPublic:false})
+  const userInPrivateRoomDb = privateDb.filter(room => {
+    if (room.users.includes(email)){
+      return room;
+    }
+  })
+  res.status(200).send({publicDb, userInPrivateRoomDb})
+});
+
+app.get('/room/:id', async (req,res) => {
+  console.log(req.params.id)
+  let roomDb = await ChatRoom.findById(req.params.id)
+  res.status(200).send(roomDb)
+})
+
+app.post("/create", async (req,res) => {
+  let result = await ChatRoom.create(req.body)
+  console.log("NEW ROOM:",result)
+  res.status(200).send(result)
+})
 
 io.on("connection", (socket) => {
-  console.log("user connected");
+  console.log("user connected to ", socket.handshake.query.chatROOM);
+  socket.on('disconnect', () => {
+    socket.leave(socket.handshake.query.chatROOM)
+    console.log('user disconnected from', socket.handshake.query.chatROOM);
+  });
 
-  socket.on("chat message", (msg) => {
-    console.log("Message: ", msg);
+  // socket.join(socket.handshake.query.chatROOM)
+
+  socket.on("chat message", async (msg) => {
+    console.log("message room id before DB UPDATE:", msg.ChatRoomID)
+    socket.broadcast.emit("new message", msg)
+    await ChatRoom.findByIdAndUpdate({_id: msg.ChatRoomID},{$push:{messages: msg}})
   });
 });
 
